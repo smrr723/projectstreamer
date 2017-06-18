cf624d413ac3948fbd0703890d2e0a166c074fcb
Scott Murray <rai.five@gmail.com>
2017-06-18 15:55:46 +0100

    Delete items

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,7 @@ var router = express.Router({
 /* GET home page. */
 router.get("/", function(req, res) {
   res.render("index");
-})
-;router.get("/hello", function(req, res) {
-  res.render("hello");
 });
 
-router.get("/1", function(req, res) {
-  res.render("index1");
-});
-
-router.get("/test/:content", function(req, res) {
-  res.render("content", {
-    content: req.params.content
-  });
-});
 
 module.exports = router;
